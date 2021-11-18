002e93d40e9f285cc55c00a04e059882a04ad470
fahadalduaij <fahad_osama@hotmail.com>
2021-11-18 21:24:57 +0300

    adding image for user

--- db/models/User.js
+++ db/models/User.js
@@ -1,8 +1,12 @@
 const { model, Schema } = require("mongoose");
 
 const UserSchema = Schema({
-  username: { type: String },
-  password: String,
+	username: { type: String },
+	password: String,
+	firstName: String,
+	lastName: String,
+	email: String,
+	image: String,
 });
 
 module.exports = model("User", UserSchema);
